0abec978c2f98c7c38b88d601667f150d7424c88
Sparky678 <aryo.widandi@gmail.com>
2023-04-20 14:56:46 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./Pondasi_html/**/*.{html,js}', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
