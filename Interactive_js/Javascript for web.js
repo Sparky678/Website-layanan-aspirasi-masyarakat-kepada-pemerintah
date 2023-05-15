3c7a1211943d36153d078597e712c3f40595eda4
Sparky678 <aryo.widandi@gmail.com>
2023-05-15 09:25:01 +0700

    Menambahkan Foto, mengubah section 2

---
+++ Interactive_js/Javascript for web.js
@@ -0,0 +1,13 @@
+// Variabel
+var Name = "Mohammad Aryo Widandi"
+let Age = 18
+const Date_of_birth = "09-07-2004"
+console.log(Name)
+console.log(Age)
+console.log(Date_of_birth)
+
+// Tipe Data
+let Nilai = 92.34
+console.log(typeof(Name))
+console.log(typeof(Age))
+console.log(typeof(Nilai))
